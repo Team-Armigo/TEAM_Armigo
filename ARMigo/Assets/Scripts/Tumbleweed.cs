c2426ccff5e9a388fef4c8202232aedaff2500ac
Jeremy <parkkyoungrun@gmail.com>
2022-07-16 23:11:05 +0900

    Update Assest
    Feat: "똥 피하기 게임 구현"
    
    3D 플랫폼 내 2D 배경 구현
    - Canvas로 안드로이드 화면 구현
    - UI의 image로 모든 게임 오브젝트 생성
    
    똥 피하기 기능 구현
    - 장애물 생성
    - 메인 캐릭터 생성
    - AR 카메라의 실시간 좌표로 캐릭터 움직임
    
    AR 기능 구현
    - Render Texture를 통한 AR 카메라 위치 설정
    - AR Camera의 특정 index값의 x,y 좌표 불러오기
    
    Resolves: #TBD
    Ref: #TBD
    Related to: #TBD

---
+++ ARMigo/Assets/Scripts/Tumbleweed.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tumbleweed : MonoBehaviour
+{
+    // Adding Variables
+    [SerializeField]
+    private float moveForce = 10f;
+
+    private float GRAVITY = 9.8f;
+
+    private float Velocity = 0f;
+    
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            Destroy(this.gameObject);
+
+        }
+
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 current = this.transform.position;
+
+        Velocity += GRAVITY * Time.deltaTime;
+
+        current.y -= Velocity * Time.deltaTime * moveForce;
+        this.transform.position = current;
+    }
+}
