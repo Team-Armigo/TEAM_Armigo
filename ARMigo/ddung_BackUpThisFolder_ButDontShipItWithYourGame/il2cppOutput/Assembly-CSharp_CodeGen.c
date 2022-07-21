5ea833e77c15534ea11257ebc407ebda2390ef7e
Jeremy <parkkyoungrun@gmail.com>
2022-07-21 12:09:01 +0900

    Scene Rune Updated
    Feat: "AR기능, Player 스크립트 구분"
    
    - 본래 Player 스크립트에 붙어있던 AR 기능들 분리 시켜 놓기
    - 플레이어, 그라운드 Collide를 통한 Score, Health 개념 구현
    - Player 움직임 부드럽게 구현, 얼굴 정중앙 위치할 시 캐릭터 멈춤 구현
    
    Resolves: #TBD
    Ref: #TBD
    Related to: #TBD

---
+++ ARMigo/ddung_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,181 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Single ARFaceDetector::get_xCor()
+extern void ARFaceDetector_get_xCor_mABAD6DF48013AE4DCA52DB06529A444625AE68B8 (void);
+// 0x00000002 System.Void ARFaceDetector::set_xCor(System.Single)
+extern void ARFaceDetector_set_xCor_m2F2F5D19A1DA2811889A9ED5BB7B8E680DF66485 (void);
+// 0x00000003 System.Single ARFaceDetector::get_yCor()
+extern void ARFaceDetector_get_yCor_m1E5C3104E0BB692D199CC05681E11D06C858CEF3 (void);
+// 0x00000004 System.Void ARFaceDetector::set_yCor(System.Single)
+extern void ARFaceDetector_set_yCor_mB9E84D640FC6E19702F70F22FB9AE1EE88FE889C (void);
+// 0x00000005 System.Void ARFaceDetector::Start()
+extern void ARFaceDetector_Start_m0ECC836032F5E5B6A96547076E4366DF24B511DB (void);
+// 0x00000006 System.Void ARFaceDetector::Update()
+extern void ARFaceDetector_Update_m4EA52533617BF4D43E4F5657ECB170BF7C95F9B5 (void);
+// 0x00000007 System.Void ARFaceDetector::OnDetectFaceAll(UnityEngine.XR.ARFoundation.ARFacesChangedEventArgs)
+extern void ARFaceDetector_OnDetectFaceAll_m8257452250B13E2163DBACDA811E4FC903C685C8 (void);
+// 0x00000008 System.Void ARFaceDetector::.ctor()
+extern void ARFaceDetector__ctor_m08FD084A954B039E827D0D0139F3F5457B224A94 (void);
+// 0x00000009 System.Void Collector::OnTriggerEnter(UnityEngine.Collider)
+extern void Collector_OnTriggerEnter_m735E019907A59FCB4562207E026E4ABB247B0B46 (void);
+// 0x0000000A System.Void Collector::.ctor()
+extern void Collector__ctor_m95811C0635826944B2AD5CB5BF8A64A062A8F1D0 (void);
+// 0x0000000B System.Void Player::Awake()
+extern void Player_Awake_m512A28E1559EB8AEEB2E1DB873F9F99FCC96BA67 (void);
+// 0x0000000C System.Void Player::Start()
+extern void Player_Start_mC31CF4F40DDEA35C5E39E8C43EC37284AE7C453D (void);
+// 0x0000000D System.Void Player::Update()
+extern void Player_Update_m95E134A5EF1B5164EA281F61D7FA436F59BE3C9F (void);
+// 0x0000000E System.Void Player::OnTriggerEnter(UnityEngine.Collider)
+extern void Player_OnTriggerEnter_m51AE06A67257AFF378594339C9D53201B6104650 (void);
+// 0x0000000F System.Void Player::OnCollisionEnter(UnityEngine.Collision)
+extern void Player_OnCollisionEnter_mD8FFD58F1755BCB20B74ECDD0BB365677FA639EE (void);
+// 0x00000010 System.Void Player::.ctor()
+extern void Player__ctor_m0A83E0706592FC871B0CF188B37AFC6649F3D85D (void);
+// 0x00000011 System.Void TumbleGenerator::Start()
+extern void TumbleGenerator_Start_m225F122B5C6272D7172C3D841AE2CE995A21F2A3 (void);
+// 0x00000012 System.Void TumbleGenerator::Update()
+extern void TumbleGenerator_Update_mD1095176B5AB642155B764552DD91A5689CE82C0 (void);
+// 0x00000013 System.Collections.IEnumerator TumbleGenerator::CreatepoopRoutine()
+extern void TumbleGenerator_CreatepoopRoutine_m534E3479FFF8FDD7F792087728A8B1E585595222 (void);
+// 0x00000014 System.Void TumbleGenerator::.ctor()
+extern void TumbleGenerator__ctor_m82E98A342B17CB1A8CF6D30208E4C81D75EB91DC (void);
+// 0x00000015 System.Void TumbleGenerator/<CreatepoopRoutine>d__3::.ctor(System.Int32)
+extern void U3CCreatepoopRoutineU3Ed__3__ctor_mD1C6CAF782CFA979575D1C42B0F148157E1D3253 (void);
+// 0x00000016 System.Void TumbleGenerator/<CreatepoopRoutine>d__3::System.IDisposable.Dispose()
+extern void U3CCreatepoopRoutineU3Ed__3_System_IDisposable_Dispose_m8C7FA7A0790BF56C6C88C062122630C4AB90B70A (void);
+// 0x00000017 System.Boolean TumbleGenerator/<CreatepoopRoutine>d__3::MoveNext()
+extern void U3CCreatepoopRoutineU3Ed__3_MoveNext_mEA91BE5BFD5DE0A99129BE80BE1254C8F72D93A4 (void);
+// 0x00000018 System.Object TumbleGenerator/<CreatepoopRoutine>d__3::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CCreatepoopRoutineU3Ed__3_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mE5C5DC91EBBE861CC7696D9C79022105DF3BC1EE (void);
+// 0x00000019 System.Void TumbleGenerator/<CreatepoopRoutine>d__3::System.Collections.IEnumerator.Reset()
+extern void U3CCreatepoopRoutineU3Ed__3_System_Collections_IEnumerator_Reset_m06B7885D4CEF68B219DD6B60F716A89B521462FD (void);
+// 0x0000001A System.Object TumbleGenerator/<CreatepoopRoutine>d__3::System.Collections.IEnumerator.get_Current()
+extern void U3CCreatepoopRoutineU3Ed__3_System_Collections_IEnumerator_get_Current_mB580CAC49C9B83EF7862154B2D4466C958AC4533 (void);
+// 0x0000001B System.Void Tumbleweed::OnTriggerEnter(UnityEngine.Collider)
+extern void Tumbleweed_OnTriggerEnter_mD5234F5E8F4D7A31D83961A34A27504E48ED2957 (void);
+// 0x0000001C System.Void Tumbleweed::Start()
+extern void Tumbleweed_Start_mDFBE949DAE62CB95CF00992738E2EB4579060242 (void);
+// 0x0000001D System.Void Tumbleweed::Update()
+extern void Tumbleweed_Update_m3C4C507290938CBDF78306345E196521AE9E10E6 (void);
+// 0x0000001E System.Void Tumbleweed::.ctor()
+extern void Tumbleweed__ctor_mFCF518E9E06DF1E44B64CA418D1D6FF371AC51A8 (void);
+// 0x0000001F System.Void ReadmeVE2::.ctor()
+extern void ReadmeVE2__ctor_m664D55081C63E6A22B07F1729528511EA3E819D1 (void);
+// 0x00000020 System.Void ReadmeVE2/Section::.ctor()
+extern void Section__ctor_m837C755929457F1368E5C6FF3A6AFDCC1F4BD237 (void);
+// 0x00000021 System.Void MoveBackground::Start()
+extern void MoveBackground_Start_m0E6DAF1FAA3D5522DD72C58EBC1C9A952F6FF04D (void);
+// 0x00000022 System.Void MoveBackground::Update()
+extern void MoveBackground_Update_mE704EEB146565F59B60DFD46B6E760A38213320C (void);
+// 0x00000023 System.Void MoveBackground::.ctor()
+extern void MoveBackground__ctor_mBE84294DFCA64A2344B2C45597DA6C80064FF565 (void);
+static Il2CppMethodPointer s_methodPointers[35] = 
+{
+	ARFaceDetector_get_xCor_mABAD6DF48013AE4DCA52DB06529A444625AE68B8,
+	ARFaceDetector_set_xCor_m2F2F5D19A1DA2811889A9ED5BB7B8E680DF66485,
+	ARFaceDetector_get_yCor_m1E5C3104E0BB692D199CC05681E11D06C858CEF3,
+	ARFaceDetector_set_yCor_mB9E84D640FC6E19702F70F22FB9AE1EE88FE889C,
+	ARFaceDetector_Start_m0ECC836032F5E5B6A96547076E4366DF24B511DB,
+	ARFaceDetector_Update_m4EA52533617BF4D43E4F5657ECB170BF7C95F9B5,
+	ARFaceDetector_OnDetectFaceAll_m8257452250B13E2163DBACDA811E4FC903C685C8,
+	ARFaceDetector__ctor_m08FD084A954B039E827D0D0139F3F5457B224A94,
+	Collector_OnTriggerEnter_m735E019907A59FCB4562207E026E4ABB247B0B46,
+	Collector__ctor_m95811C0635826944B2AD5CB5BF8A64A062A8F1D0,
+	Player_Awake_m512A28E1559EB8AEEB2E1DB873F9F99FCC96BA67,
+	Player_Start_mC31CF4F40DDEA35C5E39E8C43EC37284AE7C453D,
+	Player_Update_m95E134A5EF1B5164EA281F61D7FA436F59BE3C9F,
+	Player_OnTriggerEnter_m51AE06A67257AFF378594339C9D53201B6104650,
+	Player_OnCollisionEnter_mD8FFD58F1755BCB20B74ECDD0BB365677FA639EE,
+	Player__ctor_m0A83E0706592FC871B0CF188B37AFC6649F3D85D,
+	TumbleGenerator_Start_m225F122B5C6272D7172C3D841AE2CE995A21F2A3,
+	TumbleGenerator_Update_mD1095176B5AB642155B764552DD91A5689CE82C0,
+	TumbleGenerator_CreatepoopRoutine_m534E3479FFF8FDD7F792087728A8B1E585595222,
+	TumbleGenerator__ctor_m82E98A342B17CB1A8CF6D30208E4C81D75EB91DC,
+	U3CCreatepoopRoutineU3Ed__3__ctor_mD1C6CAF782CFA979575D1C42B0F148157E1D3253,
+	U3CCreatepoopRoutineU3Ed__3_System_IDisposable_Dispose_m8C7FA7A0790BF56C6C88C062122630C4AB90B70A,
+	U3CCreatepoopRoutineU3Ed__3_MoveNext_mEA91BE5BFD5DE0A99129BE80BE1254C8F72D93A4,
+	U3CCreatepoopRoutineU3Ed__3_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mE5C5DC91EBBE861CC7696D9C79022105DF3BC1EE,
+	U3CCreatepoopRoutineU3Ed__3_System_Collections_IEnumerator_Reset_m06B7885D4CEF68B219DD6B60F716A89B521462FD,
+	U3CCreatepoopRoutineU3Ed__3_System_Collections_IEnumerator_get_Current_mB580CAC49C9B83EF7862154B2D4466C958AC4533,
+	Tumbleweed_OnTriggerEnter_mD5234F5E8F4D7A31D83961A34A27504E48ED2957,
+	Tumbleweed_Start_mDFBE949DAE62CB95CF00992738E2EB4579060242,
+	Tumbleweed_Update_m3C4C507290938CBDF78306345E196521AE9E10E6,
+	Tumbleweed__ctor_mFCF518E9E06DF1E44B64CA418D1D6FF371AC51A8,
+	ReadmeVE2__ctor_m664D55081C63E6A22B07F1729528511EA3E819D1,
+	Section__ctor_m837C755929457F1368E5C6FF3A6AFDCC1F4BD237,
+	MoveBackground_Start_m0E6DAF1FAA3D5522DD72C58EBC1C9A952F6FF04D,
+	MoveBackground_Update_mE704EEB146565F59B60DFD46B6E760A38213320C,
+	MoveBackground__ctor_mBE84294DFCA64A2344B2C45597DA6C80064FF565,
+};
+static const int32_t s_InvokerIndices[35] = 
+{
+	4128,
+	3418,
+	4128,
+	3418,
+	4176,
+	4176,
+	3294,
+	4176,
+	3387,
+	4176,
+	4176,
+	4176,
+	4176,
+	3387,
+	3387,
+	4176,
+	4176,
+	4176,
+	4091,
+	4176,
+	3365,
+	4176,
+	4027,
+	4091,
+	4176,
+	4091,
+	3387,
+	4176,
+	4176,
+	4176,
+	4176,
+	4176,
+	4176,
+	4176,
+	4176,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	35,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
