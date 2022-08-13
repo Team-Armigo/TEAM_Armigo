b9c11e1a8c00f9a3ba6b354ea0c645633918908c
LeeMineo <lms4882@gmail.com>
2022-08-13 16:37:01 +0900

    #10 #11 방 구현
    방 인원수 드롭다운 구현 완료
    방 생성 및 방 퇴장 구현 완료

---
+++ ARMigo/Assets/Scripts/min/RoomData.cs
@@ -0,0 +1,154 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+
+public class RoomData : MonoBehaviourPunCallbacks
+{
+
+    private TMP_Text RoomInfoText;
+    private RoomInfo _roomInfo;
+    
+
+    public TMP_InputField userIdText;
+    public TMP_InputField roomNameText;
+    public TMP_Dropdown maxNum;
+    public TMP_InputField passWord;
+
+    //public static TMP_InputField name = roomNameText;
+
+    //byte maxPlayers = byte.Parse(maxNum.options[maxNum.value].text);
+
+
+    //룸 목록 저장하기 위한 딕셔너리 자료형 
+    private Dictionary<string, GameObject> roomDict = new Dictionary<string, GameObject>();
+    
+    // 룸을 표시할 프리팹 
+    public GameObject roomPrefab;
+    // Room 프리팹이 차일드화 시킬 부모 객체 
+    public Transform scrollContent;
+
+    public RoomInfo RoomInfo
+    {
+        get
+        {
+            return _roomInfo;
+        }
+
+        set
+        {
+            _roomInfo = value;
+            RoomInfoText.text = $"{_roomInfo.Name} ({_roomInfo.PlayerCount}/{_roomInfo.MaxPlayers})";
+            GetComponent<UnityEngine.UI.Button>().onClick.AddListener(() => OnEnterRoom(_roomInfo.Name));
+        }
+    }
+/*
+    void Awake()
+    {
+        RoomInfoText = GetComponentInChildren<TMP_Text>();
+        userIdText = GameObject.Find("InputField (TMP) - Nickname").GetComponent<TMP_InputField>();
+        
+    }
+*/
+    void OnEnterRoom(string roomName)
+    {
+        RoomOptions ro = new RoomOptions();
+        ro.IsOpen = true;
+        ro.IsVisible = true;
+        ro.MaxPlayers = 10;
+
+        PhotonNetwork.NickName = userIdText.text;
+        PhotonNetwork.JoinOrCreateRoom(roomName, ro, TypedLobby.Default);
+    }
+
+
+    public void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        GameObject tempRoom = null;
+        foreach(var room in roomList)
+        {
+            // 룸이 삭제된 경우
+            if (room.RemovedFromList == true)
+            {
+                roomDict.TryGetValue(room.Name, out tempRoom);
+                Destroy(tempRoom);
+                roomDict.Remove(room.Name);
+            }
+            // 룸 정보가 갱신 (변경 ) 된 경우 
+            else
+            {
+                // 룸이 처음 생성된 경우
+                if (roomDict.ContainsKey(room.Name) == false)
+                {
+                    GameObject _room = Instantiate(roomPrefab, scrollContent);
+                    _room.GetComponent<RoomData>().RoomInfo = room;
+                    roomDict.Add(room.Name, _room);
+                }
+                // 룸 정보를 갱신하는 경우
+                else
+                {
+                    roomDict.TryGetValue(room.Name, out tempRoom);
+                    tempRoom.GetComponent<RoomData>().RoomInfo = room;
+                }
+            }
+        }
+    }
+
+
+
+    public void RoomCreate()
+    {
+        byte maxPlayers = byte.Parse(maxNum.options[maxNum.value].text); // 드롭다운에서 값 얻어오기.
+        
+        RoomOptions ro = new RoomOptions();
+        //ro.RoomName = roomNameText;
+        ro.MaxPlayers = maxPlayers;
+        ro.IsOpen = true;
+        ro.IsVisible = true;
+
+        // 인풋필드가 비어있으면
+        if(string.IsNullOrEmpty(roomNameText.text))
+        {
+            //랜덤 룸 이름 부여
+            roomNameText.text = $"ROOM_{Random.Range(1, 100):000}";
+        }
+        //생성과 동시에 입장.성공. 
+        PhotonNetwork.CreateRoom(roomNameText.text, ro);
+        Debug.Log("코드가 작동은 되었다.-Room");
+        
+    }
+    //이코드 작동 
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("03.방 생성 완료-Room");
+    }
+    //이코드 작동 
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("04.방 입장 완료. 방 씬으로 옮김.- room");
+        SceneManager.LoadScene("RoomMain");
+
+        if(PhotonNetwork.IsMasterClient)
+        {
+            //PhotonNetwork.LoadLevel("Level_1");
+            Debug.Log("IF");
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
