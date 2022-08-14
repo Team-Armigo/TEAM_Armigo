4e80fe96a24d64047046a50a84d7fa5ea3047364
LeeMineo <lms4882@gmail.com>
2022-08-14 18:44:24 +0900

    #11 코드 정리
    필요없는 코드 지우고 필요한 코드만 남기고 정리

--- ARMigo/Assets/Scripts/min/ListManager.cs
+++ ARMigo/Assets/Scripts/min/ListManager.cs
@@ -6,13 +6,52 @@ using Photon.Realtime;
 using UnityEngine.UI;
 using TMPro;
 
-public class ListManager : MonoBehaviour
+public class ListManager : MonoBehaviourPunCallbacks
 {
 
     public string userId;
 
+    private TMP_Text RoomInfoText;
+    private RoomInfo _roomInfo;
+    //룸 목록 저장하기 위한 딕셔너리 자료형 
+    private static Dictionary<string, GameObject> roomDict = new Dictionary<string, GameObject>();
+    
+    public GameObject roomPrefab;
+    public Transform scrollContent;
 
-/*
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        GameObject tempRoom = null;
+        foreach(var room in roomList)
+        {
+            // 룸이 삭제된 경우
+            if (room.RemovedFromList == true)
+            {
+                roomDict.TryGetValue(room.Name, out tempRoom);
+                Destroy(tempRoom);
+                roomDict.Remove(room.Name);
+            }
+            // 룸 정보가 갱신 (변경 ) 된 경우 
+            else
+            {
+                // 룸이 처음 생성된 경우
+                if (roomDict.ContainsKey(room.Name) == false)
+                {
+                    GameObject _room = Instantiate(roomPrefab, scrollContent);
+                    _room.GetComponent<RoomData>().RoomInfo = room;
+                    roomDict.Add(room.Name, _room);
+                }
+                // 룸 정보를 갱신하는 경우
+                else
+                {
+                    roomDict.TryGetValue(room.Name, out tempRoom);
+                    tempRoom.GetComponent<RoomData>().RoomInfo = room;
+                }
+            }
+        }
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,5 +64,5 @@ public class ListManager : MonoBehaviour
         
     }
 
-    */
+   
 }
